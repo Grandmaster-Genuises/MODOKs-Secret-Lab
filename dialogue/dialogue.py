2b71f2658d10c59da826f389d716eda58963bd11
NeuroAssassin <wrbrown70@yahoo.com>
2018-09-21 08:57:41 -0400

    Set configurable settings

--- dialogue/dialogue.py
+++ dialogue/dialogue.py
@@ -1,5 +1,5 @@
 import discord
-from redbot.core import commands
+from redbot.core import commands, checks, Config
 import random
 
 class Dialogue:
@@ -7,9 +7,49 @@ class Dialogue:
     def __init__(self, bot):
         self.bot = bot
         self.insults = ["Kill!!!", "Fry!!!", "Peasant!!!", "Idiot!!!"]
+        self.config = Config.get_conf(self, identifier=2547813692)
+
+        self.config.register_guild(
+            active=True,
+            chance=10
+        )
 
     async def on_message(self, message):
         chance = random.randint(0, 100)
 
-        if chance <= 10:
+        _require = await self.config.guild(ctx.guild).chance()
+
+        if chance <= _require:
             await message.channel.send(random.choice(self.insults))
+
+
+    @commands.group()
+    async def dialogue(self, ctx):
+        """Group command for controlling dialogue by MODOK"""
+        pass
+
+    @dialogue.command()
+    @checks.admin_or_permissions(manage_server=True)
+    async def allow(self, ctx, _tf: bool):
+        await self.config.guild(ctx.guild).active.set(_tf)
+        await ctx.send(f"Dialogue active: {_tf}")
+
+    @dialogue.command()
+    @checks.admin_or_permissions(manage_server=True)
+    async def chance(self, ctx, _chance: int):
+        _is_active = self.config.guild(ctx.guild).active()
+        if not _is_active:
+            await ctx.send("Dialogue is not active")
+            return
+        await self.config.guild(ctx.guild).chance.set(_chance)
+        await ctx.send(f"Chance has been set to: {_chance}")
+
+    @dialogue.command()
+    async def info(self, ctx):
+        _active = await self.config.guild(ctx.guild).active()
+        if not _active:
+            await ctx.send("Dialogue is not active")
+            return
+        _chance = await self.config.guild(ctx.guild).chance()
+        await ctx.send(f"Dialogue is active\nChance is {_chance}")
+        
